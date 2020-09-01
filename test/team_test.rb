c2f945551006633b3db756c3d903f526945c5a12
Kate Tester <katemorris@gmail.com>
2020-08-31 21:41:03 -0600

    Adds IT2 tests.

---
+++ test/team_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/team'
+require './lib/player'
+
+
+class PlayerTest < Minitest::Test
+  def setup
+    @team = Team.new("France")
+  end
+
+  def test_it_exists
+    assert_instance_of Team, @team
+  end
+
+  def test_it_has_attributes
+    assert_equal "France", @team.country
+  end
+
+  def test_the_team_has_been_eliminated
+    assert_equal false, @team.eliminated?
+
+    team.eliminated = true
+
+    assert team.eliminated?
+  end
+
+  def test_the_team_has_players
+    assert_equal [], team.players
+
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+
+    team.add_player(mbappe)
+    team.add_player(pogba)
+
+    assert_equal [mbappe, pogba], team.players
+  end
+
+  def test_returning_players_by_position
+    assert_equal [pogba], team.players_by_position("midfielder")
+    assert_equal [], team.players_by_position("defender")
+  end
