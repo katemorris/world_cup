9fd7425d5633fffe2b17c0e88f7e8463ac01c14c
Kate Tester <katemorris@gmail.com>
2020-09-01 06:08:08 -0600

    Adds tests for world cup class and IT3.

---
+++ test/world_cup_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/world_cup'
+require './lib/team'
+require './lib/player'
+
+class WorldCupTest < Minitest::Test
+
+  def setup
+    @france = Team.new("France")
+    @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    @france.add_player(@mbappe)
+    @france.add_player(@pogba)
+
+    @croatia = Team.new("Croatia")
+    @modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    @vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    @croatia.add_player(@modric)
+    @croatia.add_player(@vida)
+
+    @world_cup = WorldCup.new(2018, [@france, @croatia])
+  end
+
+  def test_it_exists
+    assert_instance_of WorldCup, @worldcup
+  end
+
+  def test_it_has_attributes
+    assert_equal 2018, @world_cup.year
+    assert_equal [@france, @croatia], @world_cup.teams
+  end
+
+  def test_can_return_active_players_by_position
+    assert_equal [@pogba, @modric], @world_cup.active_players_by_position("midfielder")
+    croatia.eliminated = true
+    assert_equal [@pogba], @world_cup.active_players_by_position("midfielder")
+  end
+
+end   
